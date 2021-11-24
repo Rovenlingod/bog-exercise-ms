6e36df70a3b08bda49feb6261976a3e0d5202a5a
Daniil Kovalyov <kovalyovd21@gmail.com>
2021-11-24 19:15:02 +0300

    Initial commit

---
+++ src/test/java/com/example/bogexercisems/BogExerciseMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bogexercisems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogExerciseMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
