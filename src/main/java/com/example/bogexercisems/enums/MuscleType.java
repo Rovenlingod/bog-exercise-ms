bd1148dbe5c4aa832f75d0513d4ba4c8a6ff9cf1
Rovenlingod <46895241+Rovenlingod@users.noreply.github.com>
2022-03-12 18:46:04 +0300

    Revert "Crud branch"

--- src/main/java/com/example/bogexercisems/enums/MuscleType.java
+++ src/main/java/com/example/bogexercisems/enums/MuscleType.java
@@ -1,5 +1,4 @@
 package com.example.bogexercisems.enums;
 
 public enum MuscleType {
-    ABS
 }
--- src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-insert into exercise (countdown, creator_id, is_public, media_file, muscle_type, title, exercise_id) values (1, '05ea06a451ee11ecbf630242ac130002', true, '1.gif', 'ABS', 'testTitle1', '04f266fd-14ba-4303-9b61-1da5acb15021');
-insert into exercise (countdown, creator_id, is_public, media_file, muscle_type, title, exercise_id) values (1, '05ea06a451ee11ecbf630242ac130002', true, '1.gif', 'ABS', 'testTitle2', '099410f9-bef5-4a0b-a6c0-3dd3fc5bbfaf');
-insert into exercise (countdown, creator_id, is_public, media_file, muscle_type, title, exercise_id) values (1, '05ea06a451ee11ecbf630242ac130002', true, '1.gif', 'ABS', 'testTitle3', 'dccabe60-d07d-4326-827e-820f66f6d66d');
-insert into exercise (countdown, creator_id, is_public, media_file, muscle_type, title, exercise_id) values (1, '05ea06a451ee11ecbf630242ac130002', true, '1.gif', 'ABS', 'testTitle4', '574a3024-e43a-4a8f-8a06-849faed4aa10');
-insert into exercise (countdown, creator_id, is_public, media_file, muscle_type, title, exercise_id) values (1, '05ea06a451ee11ecbf630242ac130002', false, '1.gif', 'ABS', 'testTitle5', '75bccce6-2b38-4b63-b882-f859dece093b');
